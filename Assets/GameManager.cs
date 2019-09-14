fcaf8de18f6044b61d1949f8713fc9f8bfd04260
James-G-Puckett <54376462+James-G-Puckett@users.noreply.github.com>
2019-09-14 15:50:46 -0500

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    
+    public Rotator rotator;
+    public Spawner spawner;
+    public Animator animator;
+    public void EndGame()
+    {
+        if (gameHasEnded)
+            return;
+
+        rotator.enabled = false;
+        spawner.enabled = false;
+        SceneManager.LoadScene("Exit");
+        animator.SetTrigger("EndGame");
+
+        gameHasEnded = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
